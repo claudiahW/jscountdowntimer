74fd16f6cc66672b78ec333fb37b2fcbf66b025b
claudiahW <murungiclaud@gmail.com>
2024-01-19 20:58:27 +0300

    add a function that adds a 0 if the time is less than 10

--- scripts.js
+++ scripts.js
@@ -28,7 +28,7 @@ const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
-let futureDate = new Date(2024,7,27,11, 30,0);
+let futureDate = new Date(2024,5,27,11, 30,0);
 
 // extract these values one by one
 
@@ -58,11 +58,27 @@ const t = futureTime - today;
 // values in ms
 const oneDay = 24 * 60 * 60 * 1000;
 const oneHour = 60 * 60 * 1000;
-const oneminute = 60 * 1000;
+const oneMinute = 60 * 1000;
 // calculate all values
 let days = t/oneDay;
 days = Math.floor(days);
-let hours = (t%oneDay) /oneHour;
+let hours = Math.floor((t % oneDay) /oneHour);
+let minutes = Math.floor((t % oneHour) / oneMinute);
+let seconds = Math.floor((t % oneMinute / 10000));
+
+// set values array
+const values = [days,hours,minutes,seconds];
+
+function format(item){
+    if(item < 10){
+        return item = `0${item}`
+    }
+    return item
+}
+
+items.forEach(function(item,index){
+  item.innerHTML = format(values[index]);  
+})
 }
 
 getRemainingTime();
