843965f5a1a04477537eff6ec164b5831877ed56
claudiahW <murungiclaud@gmail.com>
2024-01-19 19:46:02 +0300

    add a function to edit text dynamically through javascript

--- scripts.js
+++ scripts.js
@@ -28,4 +28,10 @@ const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
-let futureDate = new Date();
+let futureDate = new Date(2020,4,24,11,30,0);
+
+// extract these values one by one
+
+const year = futureDate.getFullYear();
+
+giveaway.textContent = `giveaway ends on ${year}`;
